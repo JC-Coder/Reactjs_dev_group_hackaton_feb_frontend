0d8320fa844ccca7a2307f2e99764d35221f27cb
unknown <idowu98official@gmail.com>
2023-02-23 13:04:30 +0100

    latest pr

--- src/constants/newMusic.js
+++ src/constants/newMusic.js
@@ -1,10 +1,74 @@
+import { nanoid } from "@reduxjs/toolkit"
 export const newMusic = [
     {
-        id: 1,
+        id: nanoid(),
         name: 'Birthday Song',
         artist: 'Davido',
         albumArt: 'https://images.pexels.com/photos/894156/pexels-photo-894156.jpeg?auto=compress&cs=tinysrgb&w=400',
         year: 2016,
+        requested: false,
+        status: 'not played'
+    },
+    {
+        id: nanoid(),
+        name: 'Rum & Boogie',
+        artist: 'Peruzzi',
+        albumArt: 'https://notjustok.com/wp-content/uploads/2021/04/Peruzzi.jpg',
+        year: 2021,
+        requested: false,
+        status: 'not played'
+    },
+    {
+        id: nanoid(),
+        name: 'Eclipse',
+        artist: 'Oxlade',
+        albumArt: 'https://notjustok.com/wp-content/uploads/2022/01/Oxlade-Eclipse-.jpeg',
+        year: 2021,
+        requested: false,
+        status: 'not played'
+    },
+    {
+        id: nanoid(),
+        name: 'Crazy',
+        artist: 'Tems',
+        albumArt: 'https://images.pexels.com/photos/894156/pexels-photo-894156.jpeg?auto=compress&cs=tinysrgb&w=400',
+        year: 2021,
+        requested: false,
+        status: 'not played'
+    },
+    {
+        id: nanoid(),
+        name: 'Euphoria',
+        artist: 'PsychoYP',
+        albumArt: 'https://notjustok.com/wp-content/uploads/2021/08/PsychoYP-Euphoria-scaled.jpg',
+        year: 2021,
+        requested: false,
+        status: 'not played'
+    },
+    {
+        id: nanoid(),
+        name: 'Water & Garri',
+        artist: 'Tiwa Savage',
+        albumArt: 'https://notjustok.com/wp-content/uploads/2021/08/PsychoYP-Euphoria-scaled.jpg',
+        year: 2021,
+        requested: false,
+        status: 'not played'
+    },
+    {
+        id: nanoid(),
+        name: 'Back in Office',
+        artist: 'Mayorkun',
+        albumArt: 'https://notjustok.com/wp-content/uploads/2021/08/PsychoYP-Euphoria-scaled.jpg',
+        year: 2021,
+        requested: false,
+        status: 'not played'
+    },
+    {
+        id: nanoid(),
+        name: 'Back in Office',
+        artist: 'Mayorkun',
+        albumArt: 'https://notjustok.com/wp-content/uploads/2021/08/PsychoYP-Euphoria-scaled.jpg',
+        year: 2021,
         requested: false,
         status: 'not played'
     },
