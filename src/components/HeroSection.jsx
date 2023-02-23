d639377f12dc4e39b168d90ef14fe0b656fc45f3
jc-coder <josephchimezie2003@gmail.com>
2023-02-23 19:35:14 +0100

    fixed now playing

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,32 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { channel } from "../config/pusher";
+import axios from "axios";
+import { apiConfig } from "../config/api";
 
-// const gradients = [
-//   "from-red-500/50",
-//   "from-slate-500/50",
-//   "from-indigo-500/50",
-//   "from-green-500/",
-//   "from-yellow-500/50",
-//   "from-purple-500/50",
-//   "from-pink-500/50",
-//   "from-blue-500/50",
-//   "from-teal-500/50",
-// ];
 
 export default function HeroSection() {
-  // const [gradient, setGradient] = useState("");
-
-  //   function getRandomGradient() {
-  //    let randomNumber = Math.floor(Math.random() * gradients.length);
-  //    setGradient(gradients[randomNumber]);
-  //   }
-
-  //   function getRandomGradient() {
-  //   let randomNumber = Math.floor(Math.random() * gradients.length);
-  //   setGradient(gradients[randomNumber]);
-  // }
-
   const [nowPlaying, setNowPlaying] = useState(null);
+  const baseUrl = apiConfig.baseUrl;
+
+  // get now playing 
+  useEffect(() => {
+    axios.get(`${baseUrl}/dj/now-playing`).then(res => {
+      setNowPlaying(res.data);
+    })
+  }, [])
 
     // update now playing
     channel.bind("now-playing", (data) => {
@@ -39,11 +26,7 @@ export default function HeroSection() {
       className={`bg-gradient-to-b relative to-black ${""} h-80 md:h-96 `}
     >
       <div className="h-80 md:h-96 custom-img bg-center bg-fixed bg-cover rounded">
-        {/* <img
-            className="h-full w-full object-cover "
-            src="https://images.pexels.com/photos/332688/pexels-photo-332688.jpeg?auto=compress&cs=tinysrgb&w=600"
-            alt=""
-          /> */}
+        
       </div>
       {nowPlaying ? (
         <h1 className="absolute animate-pulse bottom-0 font-medium text-4xl ml-auto p-2">
