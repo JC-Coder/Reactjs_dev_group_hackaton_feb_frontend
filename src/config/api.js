802f3af71bc7ab5f79bfba61ecb7ac50daf6242d
jc-coder <josephchimezie2003@gmail.com>
2023-02-23 19:35:25 +0100

    fixed now playing

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
 export const apiConfig = {
-  // baseUrl: "https://groovedeck.adaptable.app",
-  baseUrl: "http://localhost:3000",
+  baseUrl: "https://groovedeck.adaptable.app",
+  // baseUrl: "http://localhost:3000",
 };
