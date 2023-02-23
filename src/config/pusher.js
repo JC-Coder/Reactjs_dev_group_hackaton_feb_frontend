2176b77567b040bbbf8da110c714e10dde1a44af
JC-Coder <josephchimezie2003@gmail.com>
2023-02-23 12:38:25 +0100

    integration fix 02

--- src/config/pusher.js
+++ src/config/pusher.js
@@ -4,3 +4,6 @@ export const pusherInstance = new Pusher("564d257a0600fa9948fc", {
   cluster: "eu",
 });
 export var channel = pusherInstance.subscribe("gd-channel");
+export const unsubscribePusherInstance = () => {
+  pusherInstance.unsubscribe("gd-channel");
+}
