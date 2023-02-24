5c8688da6b8617ecfeeb0ede36a936b9c08bca5c
unknown <idowu98official@gmail.com>
2023-02-24 09:02:29 +0100

    logo and capitalize functions

--- src/components/MusicCardDj.jsx
+++ src/components/MusicCardDj.jsx
@@ -2,7 +2,10 @@ import { React} from "react";
 import axios from "axios";
 import { apiConfig } from "../config/api";
 import { helperFunction } from "../helper/helper";
-// import { ReactNotifications } from "react-notifications-component";
+
+export function capitalize(value) {
+  return value[0].toUpperCase() + value.substring(1)
+}
 
 export default function MusicCardDj(item) {
   const baseUrl = apiConfig.baseUrl;
@@ -10,8 +13,8 @@ export default function MusicCardDj(item) {
   function markAsNowPlaying(item) {
     axios
       .post(`${baseUrl}/dj/now-playing`, {
-        name: item.name,
-        artist: item.artist,
+        name: capitalize(item.name),
+        artist: capitalize(item.artist),
       })
       .then((res) => {
         if (res.status == 200) {
@@ -49,8 +52,8 @@ export default function MusicCardDj(item) {
         />
       </div>
       <div className="pt-2">
-        <h2 className="text-white font-medium">{item.name}</h2>
-        <span className="text-sm text-gray-400">{item.artist}</span>
+        <h2 className="text-white font-medium">{capitalize(item.name)}</h2>
+        <span className="text-sm text-gray-400">{capitalize(item.artist)}</span>
         <button
           className="font-medium block mt-2 text-xs active:scale-90 transition bg-white text-[#1e1e1e] hover:text-white hover:bg-blue-400 px-2 py-1 rounded"
           onClick={() => markAsNowPlaying(item)}
